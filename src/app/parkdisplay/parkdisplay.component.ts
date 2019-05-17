0dd4a49b49f7adab03ac04abee9cb1fc8c41032e
Bill Hall <bill.hall@libtax.com>
2019-05-17 18:20:51 -0400

    Changed layout

--- src/app/parkdisplay/parkdisplay.component.ts
+++ src/app/parkdisplay/parkdisplay.component.ts
@@ -13,47 +13,84 @@ import { Observable } from 'rxjs';
 export class ParkdisplayComponent implements OnInit, AfterViewInit {
   public lat: number;
   public lng: number;
-  parkList: Observable<string[]>;
-  
+  public parkList: google.maps.places.PlaceResult[];
+  public markers: google.maps.Marker[] = [];
+  private gmap: google.maps.Map;
 
   ngOnInit() {
     // Initialize the map
-    let map: google.maps.Map;
     const norfolk = new google.maps.LatLng(36.9308009, -76.3097602);
 
-    map = new google.maps.Map(
+    this.gmap = new google.maps.Map(
       document.getElementById('map'), { center: norfolk, zoom: 15 });
 
-    this.getParkMap(map, norfolk);
-
-
-
+    this.getParkMap(this.gmap, norfolk)
+      .then(results => {
+        this.parkList = results;
+        results.forEach(x =>
+          this.markers.push(
+            new google.maps.Marker({
+              map: this.gmap,
+              position: x.geometry.location,
+              title: x.name
+            })
+          ))
+      });
   }
 
   ngAfterViewInit() {
-
   }
+
   constructor() { }
 
-  getParkMap(gmap, LatLng) {
-    const service = new google.maps.places.PlacesService(gmap);
-    service.nearbySearch(
-      { location: LatLng, radius: 5000, type: 'park' },
-      function (results, status, pagination) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            var marker = new google.maps.Marker({
-              map: gmap,
-              position: results[i].geometry.location,
-              title: results[i].name
-            });
-            gmap.setCenter(results[0].geometry.location);
+  public onClick(location) {
+    // clear markers
+    this.markers.forEach(x => x.setMap(null));
+
+    this.getLocation(location)
+      .then(result => {
+
+        this.getParkMap(this.gmap, result)
+          .then(results => {
+            this.parkList = results;
+            results.forEach(x =>
+              new google.maps.Marker({
+                map: this.gmap,
+                position: x.geometry.location,
+                title: x.name
+              })
+            )
+          });
+
+      });
+  }
+
+  private getLocation(address): Promise<google.maps.GeocoderResult> {
+    return new Promise((resolve, reject) =>
+      new google.maps.Geocoder().geocode(
+        { address },
+        (results, status) => {
+          if (status !== google.maps.GeocoderStatus.OK) {
+            reject();
           }
+          resolve(results[0]);
         }
-      }
+      )
     );
   }
-  createMarker(places) { }
 
-
+  private getParkMap(gmap, LatLng): Promise<google.maps.places.PlaceResult[]> {
+    // promisefy 
+    return new Promise((resolve, reject) =>
+      new google.maps.places.PlacesService(gmap)
+        .nearbySearch(
+          { location: LatLng, radius: 5000, type: 'park' },
+          (results, status, pagination) => {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+              reject();
+            }
+            resolve(results);
+          })
+    );
+  }
 }
